4072098a7c38b436b002fd2f6c1d7e569a088423
Maxime <stocletm4xime@gmail.com>
2016-01-16 04:23:28 +0100

    Modif methode init.php dans membre

--- modules/membre/init.php
+++ modules/membre/init.php
@@ -2,8 +2,8 @@
 // pareil les méthode init sont tjs les meme dans chaque modules
 if (!isset($_SESSION["id_user"])) {
     $_SESSION["id_erreur"] = "401";
-    $_SESSION["erreur_message"] = 'Une connexion est requise pour continuer la navigation.<br />';
-    $_SESSION["erreur_message"] .= '<a href="index.php?modules=acces&action=connexion" style="color:#008CBA;">Se connecter</a>';
+    $_SESSION["erreur_message"] = 'Veuillez vous connectez pour accéder a cette page.<br />';
+    $_SESSION["erreur_message"] .= '<a href="index.php?modules=acces&action=connexion" style="color:#333333;">Se connecter</a>';
     include('modules/erreur/init.php');
 } else {
     if (isset($_GET["action"])) {
